a318f7b09d5b9e366d48e4079df002de5a9641c7
Jack Lu <jacklu@jacklu.me>
2023-04-01 14:47:05 +0800

    define general EventHandler instead of subclassing

--- EventHandler.js
+++ EventHandler.js
@@ -1,10 +1,18 @@
-class EventHandler { }
-class Receive extends EventHandler {
-	constructor(message, statement) {
-		super();
-		this.message = message;
+/** @typedef {import('l/Expression')} Expression */
+/** @typedef {import('./Statement')} Statement */
+class EventHandler {
+	/**
+	 * @param {string} type
+	 * @param {Expression} argument
+	 * @param {Statement} statement
+	 */
+	constructor(type, argument, statement) {
+		/** @type {string} */
+		this.type = type;
+		/** @type {Expression} */
+		this.argument = argument;
+		/** @type {Statement} */
 		this.statement = statement;
 	}
 }
 module.exports = EventHandler;
-module.exports.Receive = Receive;
--- semantics.js
+++ semantics.js
@@ -3,7 +3,7 @@ var EventHandler = require('./EventHandler');
 var Statement = require('./Statement');
 var semantics = grammar.extendSemantics(require('l/semantics')).extendOperation('parse', {
 	Program: statement => statement.children.map(s => s.parse()),
-	EventHandlerReceive: (on, receive, message, _do, statement) => new EventHandler.Receive(message.parse(), statement.parse()),
+	EventHandlerReceive: (on, receive, message, _do, statement) => new EventHandler('receive', message.parse(), statement.parse()),
 	StatementSend: (send, message, to, receiver, semicolon) => new Statement.Send(message.parse(), receiver.parse())
 });
 module.exports = semantics;
