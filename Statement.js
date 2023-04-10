fa60e2e84290d9b259d015992b341f228c27ef08
Jack Lu <jacklu@jacklu.me>
2023-04-10 23:47:24 +0800

    Statement.js: +type

--- Statement.js
+++ Statement.js
@@ -1,8 +1,11 @@
+/** @typedef {import('l/Expression')} Expression */
 var Statement = require('l/Statement');
 class Send extends Statement {
 	constructor(message, receiver) {
 		super('send');
+		/** @type {Expression} */
 		this.message = message;
+		/** @type {Expression} */
 		this.receiver = receiver;
 	}
 }
